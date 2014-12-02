6a2ec0c04cd580de3e8e68bd5956e32456008f7a
Rodrigo Nietiedt <rodrigo.nietiedt@vfr.com.br>
2014-12-02 18:00:18 -0200

    first commit

---
+++ teste.js
@@ -0,0 +1,184 @@
+var myCanvas = document.getElementById("myCanvas");
+var context = myCanvas.getContext("2d");
+
+
+var mapW = 10;
+var mapH = 10;
+
+var canvasW = 300;
+var canvasH = 300;
+
+var map = [[],[]];
+
+function click(e)
+{
+
+}
+
+myCanvas.addEventListener("click", click, false);
+
+function Node()
+{
+	var x = 0;
+	var y = 0;
+	var color = "#FFFF00";
+	var connections = new Array();
+}
+
+function nodeConnection()
+{
+	var x = 0;
+	var y = 0;
+	var cost = 1.0;
+}
+
+function initMap()
+{
+	for(var y=0; y< mapH; y++)
+	{
+		for(var x=0; x< mapW; x++)
+		{
+			map[x,y] = new Node();
+			map[x,y].x = x;
+			map[x,y].y = y;
+			map[x,y].color = "#AAAAAA";
+		}
+	}
+}
+function drawConnections()
+{
+	
+}
+function drawMap()
+{
+	var nodeW = (canvasW-1) / mapW;
+	var nodeH = (canvasH-1) / mapH;
+	
+	for(var y=0; y< mapH; y++)
+	{
+		for(var x=0; x< mapW; x++)
+		{
+			var node = map[x,y];
+			var x_pos = x*nodeW;
+			var y_pos = y*nodeH;
+			drawRect(x_pos+1, y_pos+1, (nodeW-1), (nodeH-1), node.color); 
+		}
+	}
+}
+
+
+
+function drawRect(x,y,w,h, color)
+{
+  context.fillStyle = color;
+  context.fillRect(x, y, w, h);
+}
+
+function drawHexagon(x,y,radius, color)
+{
+	context.fillStyle = color;
+	context.beginPath();
+	var angle = (Math.PI * 2) / 6.0;
+	for(var i =0; i< 6; i++)
+	{
+		var r_x = Math.cos(i*angle);
+		var r_y = Math.sin(i*angle);
+		var r_x2 = Math.cos((i+1)*angle);
+		var r_y2 = Math.sin((i+1)*angle);
+		context.moveTo(x+(r_x*radius), y+(r_y*radius));
+		context.lineTo(x+(r_x2*radius), y+(r_y2*radius));
+		
+		
+	}    
+	context.closePath();
+	context.fill();
+}
+var gravityAcceleration = new function()
+{
+	this.x = 0;
+	this.y = 0.001;
+}
+
+function testCollision(a, b)
+{
+	if(a.x+a.w < b.x)
+		return false;
+	if(a.x > b.x + b.w)
+		return false;
+		
+	if(a.y+a.h < b.y)
+		return false;
+	if(a.y > b.y + b.h)
+		return false;
+		
+	return true;
+}
+
+function Agent()
+{
+	this.x = 0;
+	this.y = 0;
+	this.w = 0;
+	this.h = 0;
+	this.velocity = new function()
+	{
+		this.x = 0.01;
+		this.y = 0;
+	}
+	
+	this.draw = function()
+	{
+		drawRect(this.x, this.y, this.h, this.w, "#FF0000");
+	}
+	
+	this.update = function(deltaTime)
+	{
+		this.velocity.y += gravityAcceleration.y * deltaTime;
+		this.x+=this.velocity.x * deltaTime;
+		this.y+=this.velocity.y * deltaTime;
+	}
+	
+	this.testCollision = function(collider)
+	{
+		var collided = false;
+		
+	}
+}
+function clearCanvas()
+{
+	context.clearRect ( 0 , 0 , myCanvas.width, myCanvas.height );
+}
+var agent = new Agent();
+agent.w = (canvasW-1) / mapW;
+agent.h = (canvasH-1) / mapH;
+agent.x = 2;
+agent.y = 2;
+
+
+function update(deltaTime)
+{
+	agent.update(deltaTime);
+}
+
+function draw()
+{
+	clearCanvas();
+	drawMap();
+	agent.draw();
+}
+function mainLoop()
+{
+	initMap();
+	var FPS = 30;
+	setInterval(function() {
+	  update(1000/FPS);
+	  draw();
+	}, 1000/FPS);
+	
+}
+
+
+mainLoop(true);
+
+
+
